f51b69873747f4f6dac9fa1ec142977c9ec887ff
effbdt <szadav09@gmail.com>
2025-10-01 17:40:18 +0200

    created the invert, and grayscale methods, not optimal yet

---
+++ PhotoshopApp/PhotoshopApp/Services/ImageProcessing.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PhotoshopApp.Services
+{
+	public class ImageProcessing
+	{
+		public static void InvertImage(Bitmap b)
+		{
+			BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),
+				ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+			int stride = bmData.Stride;
+			System.IntPtr Scan0 = bmData.Scan0;
+			unsafe
+			{
+				byte* p = (byte*)(void*)Scan0;
+				int nOffset = stride - b.Width * 3;
+				int nWidth = b.Width * 3;
+				int nHeight = b.Height;
+				for (int y = 0; y < nHeight; ++y)
+				{
+					for (int x = 0; x < nWidth; ++x)
+					{
+						p[0] = (byte)(255 - p[0]);
+						++p;
+					}
+					p += nOffset;
+				}
+			}
+
+			b.UnlockBits(bmData);
+		}
+
+		public static void Grayscale(Bitmap b)
+		{
+			BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),
+				ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+			int stride = bmData.Stride;
+			System.IntPtr Scan0 = bmData.Scan0;
+
+			unsafe
+			{
+				byte* p = (byte*)(void*)Scan0;
+
+				int nOffset = stride - b.Width * 3;
+
+				byte red, green, blue;
+
+				for (int y = 0; y < b.Height; y++)
+				{
+					for (int x = 0; x < b.Width; ++x)
+					{
+						blue = p[0];
+						green = p[1];
+						red = p[2];
+
+						p[0] = p[1] = p[2] = (byte)
+							(.299 * red + .587 * green + .114 * blue);
+
+						p += 3;
+					}
+					p += nOffset;
+				}
+			}
+
+			b.UnlockBits(bmData);
+		}
+	}
+
+}

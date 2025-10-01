915f6d53a269e0555b331e9e442a0d2b84da487e
effbdt <szadav09@gmail.com>
2025-10-01 20:37:36 +0200

    created the Gamma method, with a lookup table

--- PhotoshopApp/PhotoshopApp/Services/ImageProcessing.cs
+++ PhotoshopApp/PhotoshopApp/Services/ImageProcessing.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 
 namespace PhotoshopApp.Services
 {
@@ -108,15 +109,21 @@ namespace PhotoshopApp.Services
 			b.UnlockBits(bmData);
 		}
 
-		public static byte[] ContrastLookUpTable(int nContrast)
+		public static void Contrast(Bitmap b, int nContrast)
 		{
 			if (nContrast < -100) nContrast = -100;
 			if (nContrast > 100) nContrast = 100;
 
+			BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),
+				ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+			int stride = bmData.Stride;
+			System.IntPtr Scan0 = bmData.Scan0;
+
 			double contrast = (100.0 + nContrast) / 100.0;
 			contrast *= contrast;
 
-			byte[] lut = new byte[256];
+			byte[] contrastLut = new byte[256];
 
 			for (int i = 0; i < 256; i++)
 			{
@@ -129,23 +136,8 @@ namespace PhotoshopApp.Services
 				if (pixel < 0) pixel = 0;
 				if (pixel > 255) pixel = 255;
 
-				lut[i] = (byte)pixel;
+				contrastLut[i] = (byte)pixel;
 			}
-			return lut;
-		}
-
-		public static void Contrast(Bitmap b, int nContrast)
-		{
-			if (nContrast < -100) nContrast = -100;
-			if (nContrast > 100) nContrast = 100;
-
-			BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),
-				ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-
-			int stride = bmData.Stride;
-			System.IntPtr Scan0 = bmData.Scan0;
-
-			byte[] lUT = ContrastLookUpTable(nContrast);
 
 			unsafe
 			{
@@ -156,9 +148,66 @@ namespace PhotoshopApp.Services
 				{
 					for (int x = 0; x < b.Width; ++x)
 					{
-						p[0] = lUT[p[0]];
-						p[1] = lUT[p[1]];
-						p[2] = lUT[p[2]];
+						p[0] = contrastLut[p[0]];
+						p[1] = contrastLut[p[1]];
+						p[2] = contrastLut[p[2]];
+						p += 3;
+					}
+					p += nOffset;
+				}
+			}
+
+			b.UnlockBits(bmData);
+		}
+
+		public byte[] GammaLookUpTable(double color)
+		{
+			byte[] lut = new byte[256];
+			for (int i = 0; i < 256; i++)
+			{
+				lut[i] = (byte)Math.Min(255, (int)(
+					(255.0 * Math.Pow(i / 255.0, 1.0 / color)) + 0.5));
+			}
+			return lut;
+		}
+
+		public static void Gamma(Bitmap b, double red, double blue, double green)
+		{
+			BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),
+				ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+			int stride = bmData.Stride;
+			System.IntPtr Scan0 = bmData.Scan0;
+
+			byte[] redGamma = new byte[256];
+			byte[] greenGamma = new byte[256];
+			byte[] blueGamma = new byte[256];
+			for (int i = 0; i < 256; i++)
+			{
+				redGamma[i] = (byte)Math.Min(255, (int)(
+					(255.0 * Math.Pow(i / 255.0, 1.0 / red)) + 0.5));
+				greenGamma[i] = (byte)Math.Min(255, (int)(
+					(255.0 * Math.Pow(i / 255.0, 1.0 / green)) + 0.5));
+				blueGamma[i] = (byte)Math.Min(255, (int)(
+					(255.0 * Math.Pow(i / 255.0, 1.0 / blue)) + 0.5));
+			}
+
+			unsafe
+			{
+				byte* p = (byte*)(void*)Scan0;
+
+				int nOffset = stride - b.Width * 3;
+				int nWidth = b.Width * 3;
+				int nHeight = b.Height;
+
+				for (int y = 0; y < b.Height; ++y)
+				{
+					for (int x = 0; x < nWidth; ++x)
+					{
+						p[0] = blueGamma[p[0]];
+						p[1] = greenGamma[p[1]];
+						p[2] = redGamma[p[2]];
+
 						p += 3;
 					}
 					p += nOffset;
@@ -168,5 +217,4 @@ namespace PhotoshopApp.Services
 			b.UnlockBits(bmData);
 		}
 	}
-
 }

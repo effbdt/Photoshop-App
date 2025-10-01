55b10fb5e16dc61c143dc242d083f6f66c76a0e2
effbdt <szadav09@gmail.com>
2025-10-01 20:12:42 +0200

    created lookUpTable for Contrast, will optimize everything later

--- PhotoshopApp/PhotoshopApp/Services/ImageProcessing.cs
+++ PhotoshopApp/PhotoshopApp/Services/ImageProcessing.cs
@@ -107,6 +107,66 @@ namespace PhotoshopApp.Services
 
 			b.UnlockBits(bmData);
 		}
+
+		public static byte[] ContrastLookUpTable(int nContrast)
+		{
+			if (nContrast < -100) nContrast = -100;
+			if (nContrast > 100) nContrast = 100;
+
+			double contrast = (100.0 + nContrast) / 100.0;
+			contrast *= contrast;
+
+			byte[] lut = new byte[256];
+
+			for (int i = 0; i < 256; i++)
+			{
+				double pixel = i / 255.0;
+				pixel -= 0.5;
+				pixel *= contrast;
+				pixel += 0.5;
+				pixel *= 255;
+
+				if (pixel < 0) pixel = 0;
+				if (pixel > 255) pixel = 255;
+
+				lut[i] = (byte)pixel;
+			}
+			return lut;
+		}
+
+		public static void Contrast(Bitmap b, int nContrast)
+		{
+			if (nContrast < -100) nContrast = -100;
+			if (nContrast > 100) nContrast = 100;
+
+			BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),
+				ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+			int stride = bmData.Stride;
+			System.IntPtr Scan0 = bmData.Scan0;
+
+			byte[] lUT = ContrastLookUpTable(nContrast);
+
+			unsafe
+			{
+				byte* p = (byte*)(void*)Scan0;
+				int nOffset = stride - b.Width * 3;
+
+				for (int y = 0; y < b.Height; ++y)
+				{
+					for (int x = 0; x < b.Width; ++x)
+					{
+						p[0] = lUT[p[0]];
+						p[1] = lUT[p[1]];
+						p[2] = lUT[p[2]];
+						p += 3;
+					}
+					p += nOffset;
+				}
+			}
+
+			b.UnlockBits(bmData);
+		}
 	}
 
 }

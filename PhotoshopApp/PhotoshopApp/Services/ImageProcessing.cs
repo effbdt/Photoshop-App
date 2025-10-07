e552160aa35d49483bfdedaba6bdcfd90723bf76
effbdt <szadav09@gmail.com>
2025-10-07 16:00:58 +0200

    created the logarightmicTransformation

--- PhotoshopApp/PhotoshopApp/Services/ImageProcessing.cs
+++ PhotoshopApp/PhotoshopApp/Services/ImageProcessing.cs
@@ -401,5 +401,44 @@ namespace PhotoshopApp.Services
 			m.Offset = 128;
 			Conv3x3(b, m);
 		}
+
+		public static void LogTransform(Bitmap b)
+		{
+			BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),
+		ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+			int stride = bmData.Stride;
+			IntPtr Scan0 = bmData.Scan0;
+
+			double c = 255.0 / Math.Log(256.0);
+
+			byte[] logLUT = new byte[256];
+			for (int i = 0; i < 256; i++)
+			{
+				logLUT[i] = (byte)(c * Math.Log(1 + i));
+			}
+
+			unsafe
+			{
+				byte* p = (byte*)(void*)Scan0;
+				int nOffset = stride - b.Width * 3;
+
+				for (int y = 0; y < b.Height; ++y)
+				{
+					for (int x = 0; x < b.Width; ++x)
+					{
+						p[0] = logLUT[p[0]];
+						p[1] = logLUT[p[1]];
+						p[2] = logLUT[p[2]];
+
+						p += 3;
+					}
+					p += nOffset;
+				}
+			}
+
+			b.UnlockBits(bmData);
+		}
+
 	}
 }
